234d2c3e5270720e253e8cc487ec015a6108ae04
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-02-03 14:01:20 +0900

    勾配

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+from __future__ import print_function
 import torch
 
-x = torch.empty(5, 3)
-print(x)
+# 計算地点の準備
+z = torch.tensor([1.0, 2.0], requires_grad=True)
+
+# f(z)の準備
+f = z[0]*2 + z[1]**2
+
+# 微分の実行
+f.backward()
+
+print(z.grad)
